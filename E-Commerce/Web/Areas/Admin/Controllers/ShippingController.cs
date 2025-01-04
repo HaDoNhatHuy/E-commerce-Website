6d0070b401fcae2c13dbc4d5ab3a7a2acd26ce12
Hà Đỗ Nhật Huy <huyhd.22th@sv.daihoclongan.edu.vn>
2025-01-04 22:02:20 +0700

    Thêm và xóa giá Ship, đồng thời cập nhật lại 1 số giao diện

--- E-Commerce/Web/Areas/Admin/Controllers/ShippingController.cs
+++ E-Commerce/Web/Areas/Admin/Controllers/ShippingController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Web.Models;
 using Web.Repository;
 
 namespace Web.Areas.Admin.Controllers
@@ -11,9 +13,44 @@ namespace Web.Areas.Admin.Controllers
         {
             _dataContext = context;
         }
-        public IActionResult Index()
+        [Route("Index")]
+        public async Task<IActionResult> Index()
         {
+            var shippingList = await _dataContext.Shippings.ToListAsync();
+            ViewBag.ShippingList = shippingList;
             return View();
+        }
+        [HttpPost]
+        [Route("StoreShipping")]
+        public async Task<IActionResult> StoreShipping(ShippingModel shippingModel, string tinh, string quan, string phuong, decimal price)
+        {
+            shippingModel.City = tinh;
+            shippingModel.District = quan;
+            shippingModel.Ward = phuong;
+            shippingModel.Price = price;
+            try
+            {
+                var existingShipping = await _dataContext.Shippings.AnyAsync(i => i.Ward == phuong && i.District == quan && i.City == tinh);
+                if (existingShipping)
+                {
+                    return Ok(new { duplicate = true, message = "Dữ liệu bị trùng lặp!" });
+                }
+                _dataContext.Shippings.Add(shippingModel);
+                await _dataContext.SaveChangesAsync();
+                return Ok(new { success = true, message = "Thêm giá vận chuyển thành công." });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "An error occurred while adding shipping.");
+            }
+        }
+        public async Task<IActionResult> Delete(int Id)
+        {
+            var shipping = await _dataContext.Shippings.FindAsync(Id);
+            _dataContext.Shippings.Remove(shipping);
+            await _dataContext.SaveChangesAsync();
+            TempData["success"] = "Đã xóa phí vận chuyển";
+            return RedirectToAction("Index","Shipping");
         }
     }
 }
