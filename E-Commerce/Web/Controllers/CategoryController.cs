92a8b39b5d677f177323e20a3e24b19bf5233218
Hà Đỗ Nhật Huy <huyhd.22th@sv.daihoclongan.edu.vn>
2025-01-06 09:10:35 +0700

    Hoàn thành lọc giá

--- E-Commerce/Web/Controllers/CategoryController.cs
+++ E-Commerce/Web/Controllers/CategoryController.cs
@@ -12,13 +12,52 @@ namespace Web.Controllers
         {
             _dataContext = context;
         }
-        public async Task<IActionResult> Index(string Slug = "")
+        public async Task<IActionResult> Index(string Slug = "", string sort_by = "", string startprice = "", string endprice = "")
         {
             CategoryModel category = _dataContext.Categories.Where(i => i.Slug == Slug).FirstOrDefault();
             if (category == null)
                 return RedirectToAction("Index");
-            var productsByCategory = _dataContext.Products.Where(i => i.CategoryId == category.Id);
-            return View(await productsByCategory.OrderByDescending(i => i.Id).ToListAsync());
+            ViewBag.Slug = Slug;
+            //lấy tất cả sản phẩm
+            IQueryable<ProductModel> productsByCategory = _dataContext.Products.Where(i => i.CategoryId == category.Id);
+            var count = await productsByCategory.CountAsync();
+            if (count > 0)
+            {
+                if (sort_by == "price_increase")
+                {
+                    productsByCategory = productsByCategory.OrderBy(i => i.Price);
+                }
+                else if (sort_by == "price_decrease")
+                {
+                    productsByCategory = productsByCategory.OrderByDescending(i => i.Price);
+                }
+                else if (sort_by == "price_newest")
+                {
+                    productsByCategory = productsByCategory.OrderByDescending(i => i.Id);
+                }
+                else if (sort_by == "oldest_newest")
+                {
+                    productsByCategory = productsByCategory.OrderBy(i => i.Id);
+                }
+                else if (startprice != "" && endprice != "")
+                {
+                    decimal startPriceValue;
+                    decimal endPriceValue;
+                    if (decimal.TryParse(startprice, out startPriceValue) && decimal.TryParse(endprice, out endPriceValue))
+                    {
+                        productsByCategory = productsByCategory.Where(i => i.Price >= startPriceValue && i.Price <= endPriceValue);
+                    }
+                    else
+                    {
+                        productsByCategory = productsByCategory.OrderByDescending(i => i.Id);
+                    }
+                }
+                else
+                {
+                    productsByCategory = productsByCategory.OrderByDescending(i => i.Id);
+                }
+            }
+            return View(await productsByCategory.ToListAsync());
         }
     }
 }
