bdec4747e372dfca0dfa62c5af37975a7b07943f
Hà Đỗ Nhật Huy <huyhd.22th@sv.daihoclongan.edu.vn>
2025-04-23 22:18:34 +0700

    Tiếp tục chỉnh sửa giao diện

--- E-Commerce/Web/Program.cs
+++ E-Commerce/Web/Program.cs
@@ -90,7 +90,7 @@ app.UseAuthorization();//Check Roles - After
 
 app.MapControllerRoute(
     name: "Areas",
-    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+    pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}");
 
 app.MapControllerRoute(
     name: "default",
